178db65eb488fc38e0c5ce36ae151a86fd23e4b1
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-12-03 18:10:25 +0100

    update

---
+++ data-types-and-operators/variables-and-assignment-operators.py
@@ -0,0 +1,25 @@
+x = 2
+y = 3
+z = 5
+
+x, y, z = 2, 3, 5
+
+#print(z)
+
+
+
+#DO THIS
+my_height = 58
+my_lat = 40
+my_long = 105
+
+#NOT THIS
+# my height = 58
+MYLONG = 40
+MyLat = 105
+
+mv_population = 74728
+#mv_population = mv_population + 4000 - 600
+mv_population += 4000 - 600
+#print(mv_population)
+
