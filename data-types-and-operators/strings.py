0ed40d21ae55af2e79a9dfd2a196c3b4f8c0947c
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-12-07 22:37:33 +0100

    update

---
+++ data-types-and-operators/strings.py
@@ -0,0 +1,42 @@
+>>> first_word = 'Hello'
+>>> second_word = 'There'
+>>> print(first_word + second_word)
+
+HelloThere
+
+>>> print(first_word + ' ' + second_word)
+
+Hello There
+
+>>> print(first_word * 5)
+
+HelloHelloHelloHelloHello
+
+>>> print(len(first_word))
+
+5
+
+
+
+ford_quote = 'Whether you think you can, or you think you can\'t--you\'re right.'
+
+
+username = "Kinari"
+timestamp = "04:50"
+url = "http://petshop.com/pets/mammals/cats"
+
+# TODO: write a log message using the variables above.
+# The message should have the same format as this one:
+# "Yogesh accessed the site http://petshop.com/pets/reptiles/pythons at 16:20."
+
+message = username + " accessed the site " + url + " at " + timestamp +"."  #replace "" with your code
+
+
+
+
+
+given_name = "William"
+middle_names = "Bradley"
+family_name = "Pitt"
+# Todo: calculate how long this name is
+name_length = len(given_name + " " + middle_names + " " + family_name) #replace `None` with your code
