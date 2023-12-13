624bb1d96652a078ab93facb31f94c400b50fa7f
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-12-13 18:46:34 +0100

    update

---
+++ data-types-and-operators/string-methods.py
@@ -0,0 +1,15 @@
+
+print("Mohammed has {} balloons".format(27))
+#####################
+
+animal = "dog" 
+action = "bite" 
+print("Does your {} {}?".format(animal, action)) 
+
+######################
+
+
+
+maria_string = "Maria loves {} and {}" 
+print(maria_string.format("math", "statistics")) 
+
