972dbab7c688a42c2ea8ab1d6973943b78dc9fcf
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-12-14 19:16:09 +0100

    update

---
+++ data-structures/dictionary.py
@@ -0,0 +1,14 @@
+# TODO: replace None with appropriate code
+# Define a dictionary, `population`, that provides information
+# on the world's largest cities. The key is the name of a city
+# (a string), and the associated value is its population in
+# millions of people.
+#   Key     |   Value
+# Shanghai  |   17.8
+# Istanbul  |   13.3
+# Karachi   |   13.0
+# Mumbai    |   12.5
+#population = {'Shanghai': 17.8, 'Istanbul': 13.3, 'Karachi': 13.0, 'Mumbai': 12.5}
+
+
+animals = {'dogs': [20, 10, 15, 8, 32, 15], 'cats': [3,4,2,8,2,4], 'rabbits': [2, 3, 3], 'fish': [0.3, 0.5, 0.8, 0.3, 1]}
